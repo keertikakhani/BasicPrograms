f60f6935e66151fd1014f94ccdd09204f38235c7
Your Name <you@example.com>
2023-02-15 15:03:22 +0530

    C# Basic Programs

---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("C# Basic Programs");
+            Console.ReadLine();
+        }
+    }
+}
